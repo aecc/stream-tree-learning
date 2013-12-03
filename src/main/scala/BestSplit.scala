4819ad53c0cf6de76c1a576788c51d6e0232b0fa
root <root@e05d082c2933.(none)>
2013-12-03 09:52:49 -0500

    BestSplit

---
+++ src/main/scala/BestSplit.scala
@@ -0,0 +1,31 @@
+import org.apache.spark.rdd.RDD
+import org.apache.spark.streaming.Duration
+import org.apache.spark.streaming.StreamingContext._
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.streaming.DStream
+
+
+object BestSplit{
+
+	def bestSplit(inputset:RDD[(Int, Int, Int)], attribset:List[String]): (Set) = {
+	
+		var rdd_size = inputset.count
+		var i_gain = Set[Any]()
+		var a = 0
+		var f_best = ""
+	
+
+		for(a <- 0 until attribset.size) {
+			if()
+		
+		}
+
+	}
+
+
+
+
+
+}
